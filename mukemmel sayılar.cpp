ad6c5ad349f6c021e2ab865d4b9bcedc947e7b37
Helinaktas <136196188+Helinaktas@users.noreply.github.com>
2023-11-16 13:02:43 +0300

    Add files via upload

---
+++ mukemmel sayılar.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sayi;
+	cout << "bir sayi giriniz" << endl;
+	cin >> sayi;
+	int bolenler = 0;
+
+	for (int i = 1; i < sayi; i++)
+	{
+		if (sayi % i == 0)
+		{
+			bolenler += i;
+		}
+	}if (sayi == bolenler)
+	{
+		cout << "girilen sayi mukemmeldir" << endl;
+	}
+	else
+	{
+		cout << "girilen sayi mukemmel degildir" << endl;
+	}
+	return 0;
+}
